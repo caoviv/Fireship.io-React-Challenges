1cdd380d3fa928f1bf8d28d602b1a3d3eb7c5767
VivianC <viviancao58@gmail.com>
2024-03-02 20:22:03 +1100

    update w lifecycle and effects challenge

--- src/components/DisplayChallenge.jsx
+++ src/components/DisplayChallenge.jsx
@@ -1,6 +1,11 @@
 function DisplayChallenge({ name }) {
   return (
-    <p>Implement a button with onclick = handleClick() function to handle state using useState()which increases a count and a previous count</p>
+    <p>
+      Implement a CountdownTimer component that implements useState() and
+      useEffect() in conjunction with setInterval to handle the timer. Make sure
+      you use the useEffect() hook to call clearTimeout() when the component is
+      destroyed.
+    </p>
   );
 }
 
