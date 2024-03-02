97c74e9a02336288668bf22dd4bc77264defdd53
VivianC <viviancao58@gmail.com>
2024-03-02 20:18:52 +1100

    implement increment button w current & prev count

--- src/components/DisplayChallenge.jsx
+++ src/components/DisplayChallenge.jsx
@@ -1,6 +1,6 @@
 function DisplayChallenge({ name }) {
   return (
-    <p>Implement a handleClick() function to handle state using useState().</p>
+    <p>Implement a button with onclick = handleClick() function to handle state using useState()which increases a count and a previous count</p>
   );
 }
 
