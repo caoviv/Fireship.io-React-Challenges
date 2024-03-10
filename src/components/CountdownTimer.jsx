3a85ab57dc38bf0398bbbd3b69d33b4efe4e844d
VivianC <viviancao58@gmail.com>
2024-03-10 18:45:54 +1100

    add pause state, timer state, pause & reset button

--- src/components/CountdownTimer.jsx
+++ src/components/CountdownTimer.jsx
@@ -1,18 +1,45 @@
 //Define a LoadingButton component. The button takes loading state, onClick, and label as props then renders the label or loader depending on the loading state.
 import { useEffect, useState } from "react";
 
-function CountdownTimer() {
+function CountdownTimer({ hr, min, sec }) {
+  const [[h, m, s], setTimer] = useState([1, 30, 0]);
+
   const [paused, setPaused] = useState(false);
-  const handleClick = () => {
+  const handlePause = () => {
     setPaused((current) => !current);
+  };
+  useEffect(() => {
+    let interval;
+    if (!paused) {
+      interval = setInterval(() => {
+        if (s > 0) {
+          setTimer((s) => s - 1);
+        } else if (m > 0) {
+          setTimer((m) => m - 1);
+        }
+      });
+    }
+    return () => clearInterval(interval);
+  }, [paused]);
+
+  const handleReset = () => {
+    setTimer({ hr, min, sec });
   };
   useEffect(() => {
     console.log(paused);
   }, [paused]);
   return (
-    <button onClick={handleClick}>
-      {paused ? <p>Start</p> : <p>Pause</p>}
-    </button>
+    <div>
+      <p>
+        {[h]}:{[m]}:{[s]}
+      </p>
+      <button onClick={handlePause}>
+        {paused ? <p>Start</p> : <p>Pause</p>}
+      </button>
+      <button onClick={handleReset}>
+        <p>Reset</p>
+      </button>
+    </div>
   );
 }
 
